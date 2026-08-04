04ad52ff3f246f90ae2cf19b632cdfd65e0d861e
HenryNebula <22852427+HenryNebula@users.noreply.github.com>
2026-08-04 07:32:09 -0400

    bump version

--- src/Translumo/AssemblyInfo.cs
+++ src/Translumo/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-[assembly: AssemblyFileVersion("1.2.2")]
-[assembly: AssemblyInformationalVersion("1.2.2")]
-[assembly: AssemblyVersion("1.2.2")]
+[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyInformationalVersion("1.2.3")]
+[assembly: AssemblyVersion("1.2.3")]
 
 [assembly: AssemblyCompany("Translumo LLM")]
 [assembly: AssemblyProduct("Translumo LLM")]
